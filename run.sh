a63ad96fa3c061d3dad83e7fdb1c9d7aba26f113
Ignacio Garro <dl1igc@darc.de>
2020-05-01 16:02:37 +0200

    refactor on raspi ubuntu

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 #ansible-playbook --user pi --ask-pass --inventory raspberrypi.local, password.yml
-ansible-playbook --user pi --ask-pass --inventory hampi.local, playbook.yml
+ansible-playbook --user ubuntu --ask-pass --inventory hampi.local, playbook.yml
